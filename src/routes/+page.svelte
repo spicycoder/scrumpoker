87f97c6e6656e1b67fd31c293c043726aa782465
SpicyCoder <spk2naveen@hotmail.com>
2023-08-26 20:30:56 +0530

    Flowbite Setup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { Button, DarkMode } from 'flowbite-svelte';
+</script>
+
+<div class="flex justify-center items-center gap-2">
+	<DarkMode />
+	<Button color="alternative">Alternative</Button>
+	<Button color="blue">Blue</Button>
+	<Button color="dark">Dark</Button>
+	<Button color="green">Green</Button>
+	<Button color="light">Light</Button>
+	<Button color="none">None</Button>
+	<Button color="primary">Primary</Button>
+	<Button color="purple">Purple</Button>
+	<Button color="red">Red</Button>
+	<Button color="yellow">Yellow</Button>
+</div>
